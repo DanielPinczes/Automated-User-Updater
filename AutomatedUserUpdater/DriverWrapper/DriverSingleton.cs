0caa497c6d922c63dbd228a5dff2727d08698e61
DanielPinczes <pinczes.daniel@stud.uni-obuda.hu>
2018-05-16 13:56:15 +0200

    Added DriverSingleton
    Only a single singleton pattern was added, including the driver factory.

--- AutomatedUserUpdater/DriverWrapper/DriverSingleton.cs
+++ AutomatedUserUpdater/DriverWrapper/DriverSingleton.cs
@@ -3,11 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
 
 namespace DriverWrapper
 {
     public class DriverSingleton
     {
+        static volatile IWebDriver driver;
+        static volatile DriverSingleton instance;
+        static object syncRoot = new object();
         
+        public IWebDriver Driver { get => driver; private set => driver = value; }
+
+        private DriverSingleton() { }
+
+        static public DriverSingleton InitSingletonInstance()
+        {
+            if (instance == null)
+            {
+                lock (syncRoot)
+                {
+                    if (instance == null) instance = new DriverSingleton();
+                }
+            }
+            return instance;
+        }
+
+        IWebDriver SetDriverTo<T>() where T: IWebDriver, new()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
